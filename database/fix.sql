15430ff7de67578d0ec91d09b94198f5a36cce08
4m1g0 <casadelblanco@gmail.com>
2011-11-13 13:14:00 +0100

    DB/ICC: Algunos daños modificados de icc, entre otros.

--- database/fix.sql
+++ database/fix.sql
@@ -71,5 +71,45 @@ UPDATE creature SET curhealth=1 WHERE id=37970;
 UPDATE creature SET curhealth=1 WHERE id=37972;
 UPDATE creature SET curhealth=1 WHERE id=37973;
 
+-- ############
+-- # ICC Work #
+-- ############
+-- APLICAR UNA SOLA VEZ
+-- mobs first part
+-- dmg
+UPDATE creature_template SET mindmg=488, maxdmg=620, attackpower=642, minrangedmg=350, maxrangedmg=530, rangedattackpower=121 WHERE entry IN (37011, 38061);
+UPDATE creature_template SET attackpower=632 WHERE entry IN (36724, 38057);
+UPDATE creature_template SET attackpower=792 WHERE entry IN (37007, 38031);
+UPDATE creature_template SET attackpower=642 WHERE entry IN (37012, 38059);
+UPDATE creature_template SET dmg_multiplier=dmg_multiplier*2, baseattacktime=1000, rangeattacktime=1000 WHERE entry IN (37011, 36724, 37007, 37012);
+UPDATE creature_template SET dmg_multiplier=dmg_multiplier*4, baseattacktime=1000, rangeattacktime=1000 WHERE entry IN (38061, 38057, 38031, 38059);
+-- Inmune
+UPDATE creature_template SET mechanic_immune_mask=667893759 WHERE entry IN (37011, 38061, 36724, 38057, 37007, 38031, 37012, 38059);
+-- respawns all ICC
+UPDATE creature SET spawntimesecs=604800 WHERE spawntimesecs < 100000 AND spawntimesecs > 30 AND map=631;
+
+-- mobs second part
+-- dmg
+UPDATE creature_template SET attackpower=326 WHERE entry IN (36808, 38075);
+UPDATE creature_template SET attackpower=782 WHERE entry IN (36805, 38076);
+UPDATE creature_template SET attackpower=726 WHERE entry IN (36811, 38072);
+UPDATE creature_template SET attackpower=330 WHERE entry IN (36807, 38073);
+UPDATE creature_template SET attackpower=669 WHERE entry IN (37890, 38393, 38628, 38629);
+UPDATE creature_template SET attackpower=642 WHERE entry IN (38009, 38398, 38630, 38631);
+UPDATE creature_template SET attackpower=642 WHERE entry IN (38135, 38395, 38634, 38635);
+UPDATE creature_template SET attackpower=608 WHERE entry IN (38136, 38396, 38632, 38633);
+UPDATE creature_template SET dmg_multiplier=14 WHERE entry IN (38009, 38398, 38630, 38631);
+UPDATE creature_template SET dmg_multiplier=14 WHERE entry IN (38135, 38395, 38634, 38635);
+UPDATE creature_template SET dmg_multiplier=14 WHERE entry IN (38136, 38396, 38632, 38633);
+UPDATE creature_template SET dmg_multiplier=19.4 WHERE entry = 36807;
+UPDATE creature_template SET dmg_multiplier=24.1 WHERE entry = 38073;
+UPDATE creature_template SET dmg_multiplier=23 WHERE entry = 36805;
+UPDATE creature_template SET dmg_multiplier=23 WHERE entry = 36805;
+UPDATE creature_template SET baseattacktime=1000, rangeattacktime=1000 WHERE entry IN (36808, 38075, 36805, 38076, 36811, 38072, 36807, 38073, 37890, 38393, 38628, 38629, 38009, 38398, 38630, 38631, 38135, 38395, 38634, 38635, 38136, 38396, 38632, 38633, 38009, 38398, 38630, 38631, 38135, 38395, 38634, 38635, 38136, 38396, 38632, 38633) AND baseattacktime =2000;
+UPDATE creature_template SET dmg_multiplier=dmg_multiplier*2 WHERE entry IN (38075, 38076, 38072, 38073, 38393, 38398, 38395, 38396, 38398, 38395, 38396);
+
+-- foso
+delete from gameobject where guid = 2642;
+
 -- [HACK] HAcemos los osos atacables por TODO el mundo para que puedan completar la mision directamente
 UPDATE creature_template SET faction_a=14, faction_h=14 WHERE entry=30174;
