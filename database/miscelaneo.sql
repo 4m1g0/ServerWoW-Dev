c1a75f19491626ce29660fa774fb47bdb1f0c528
4m1g0 <casadelblanco@gmail.com>
2011-11-13 22:42:49 +0100

    GLOBAL: Eliminado todo el data correspondiente a ytdb

--- database/miscelaneo.sql
+++ database/miscelaneo.sql
@@ -1,4 +1,2 @@
--- Evitamos que salgan mensajes de error relacionados con trinity o con ytdb
-UPDATE page_text SET `text` = '<Error> Texto no encontrado. Por favor si copnoces el texto que deberia aparecer en lugar de este mensaje comunicate con Zazu.' WHERE `text` LIKE '%ytdb%';
-UPDATE npc_text SET `text0_0` = '<Error> Texto no encontrado. Por favor si copnoces el texto que deberia aparecer en lugar de este mensaje comunicate con Zazu.' WHERE `text0_0` LIKE '%ytdb%';
+-- Evitamos que salgan mensajes de error relacionados con trinity
 UPDATE script_texts SET content_default = '<Texto to no encontrado, por favor reporta este error a Zazu>', content_loc6 = '<Texto to no encontrado, por favor reporta este error a Zazu>' WHERE entry = -1000000;
