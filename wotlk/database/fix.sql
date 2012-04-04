1eaa4dd5257be6828c7b6ccb935a0a4c0b18da0d
4m1g0 <casadelblanco@gmail.com>
2012-04-04 07:13:28 +0200

    DB/Quest: Corregida mision y scriptado npc: Halfdan Corazón Helado  by Decode

--- wotlk/database/fix.sql
+++ wotlk/database/fix.sql
@@ -101,16 +101,29 @@ VALUES
 (@ENTRY_GUARD, 0, 0, 1, 62, 0, 100, 0, @GOSSIP_1, 0, 0, 0, 56, @ITEM, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Dead Caravan Guard - On Gossip Select - Add Item Warsong Outfit'),
 (@ENTRY_GUARD, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[LCV] Dead Caravan Guard - On Gossip Select - Forced Despawn'),
 (@ENTRY_WORKER, 0, 0, 1, 62, 0, 100, 0, @GOSSIP_2, 0, 0, 0, 56, @ITEM, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Dead Caravan Worker - On Gossip Select - Add Item Warsong Outfit'),
-(@ENTRY_WORKER, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[ LCV] Dead Caravan Worker - On Gossip Select - Forced Despawn');
+(@ENTRY_WORKER, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[LCV] Dead Caravan Worker - On Gossip Select - Forced Despawn');
 
 -- Conditions
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=@GOSSIP_1;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=@GOSSIP_2;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(15, @GOSSIP_1, @GOSSIP_1, 0, 0, 9, 0, @QUEST, 0, 0, 0, 0, '', '[LCV] Only show gossip if player has quest Plan B'),
-(15, @GOSSIP_2, @GOSSIP_2, 0, 0, 9, 0, @QUEST, 0, 0, 0, 0, '', '[LCV] Only show gossip if player has quest Plan B'),
-(15, @GOSSIP_1, @GOSSIP_1, 0, 0, 2, 0, @ITEM, 10, 0, 0, 0, '', '[LCV] Only show gossip if player has less then 10 outfits'),
-(15, @GOSSIP_2, @GOSSIP_2, 0, 0, 2, 0, @ITEM, 10, 0, 0, 0, '', '[LCV] Only show gossip if player has less then 10 outfits');
+(15, @GOSSIP_1, 0, 0, 0, 9, 0, @QUEST, 0, 0, 0, 0, '', '[LCV] Only show gossip if player has quest Plan B'),
+(15, @GOSSIP_2, 0, 0, 0, 9, 0, @QUEST, 0, 0, 0, 0, '', '[LCV] Only show gossip if player has quest Plan B'),
+(15, @GOSSIP_1, 0, 0, 0, 2, 0, @ITEM, 10, 0, 1, 0, '', '[LCV] Only show gossip if player has less then 10 outfits'),
+(15, @GOSSIP_2, 0, 0, 0, 2, 0, @ITEM, 10, 0, 1, 0, '', '[LCV] Only show gossip if player has less then 10 outfits');
 
 
+-- Fix quest ¡Detén la ascensión! 11249
+SET @ENTRY := 23671;
+SET @SOURCETYPE := 0;
 
+update `creature_template` SET faction_a = 14, faction_h = 14 where entry= @ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,5000,5000,7000,8000,11,35263,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Frost Attack'),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,10000,10000,12000,13000,11,32736,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Mortal Strike'),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,7000,8000,12000,14000,11,8599,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Enrage'),
+(@ENTRY,@SOURCETYPE,4,0,0,0,100,0,3000,4000,8000,10000,11,32015,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Knockdown'),
+(@ENTRY,@SOURCETYPE,5,0,0,0,100,0,15000,20000,30000,34000,11,12169,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Shield Block'),
+(@ENTRY,@SOURCETYPE,6,0,0,0,100,0,1000,2000,18000,20000,11,19131,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Shield Charge');
