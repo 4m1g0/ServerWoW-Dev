22d8c19f7df1fdad5578a05e7c0abaf3cdd458df
4m1g0 <casadelblanco@gmail.com>
2012-04-07 03:32:21 +0200

    DB/quest: Añadido fuego al quemar el avion en quest anterior, mas blizzlike

--- wotlk/database/fix.sql
+++ wotlk/database/fix.sql
@@ -140,8 +140,11 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@NPC1, @NPC2, @NPC3) AND `so
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (@NPC1, 0, 0, 1, 8, 0, 100, 0, 46171, 0, 0, 0, 33, @NPC1, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Kill credit NPC 25845'),
+(@NPC1, 0, 1, 2, 8, 0, 100, 0, 46171, 0, 0, 0, 11, 34386, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[LCV] Active Skill Create Hellfire Fire'),
 (@NPC2, 0, 0, 1, 8, 0, 100, 0, 46171, 0, 0, 0, 33, @NPC2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Kill credit NPC 25846'),
-(@NPC3, 0, 0, 1, 8, 0, 100, 0, 46171, 0, 0, 0, 33, @NPC3, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Kill credit NPC 25847');
+(@NPC2, 0, 1, 2, 8, 0, 100, 0, 46171, 0, 0, 0, 11, 34386, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[LCV] Active Skill Create Hellfire Fire'),
+(@NPC3, 0, 0, 1, 8, 0, 100, 0, 46171, 0, 0, 0, 33, @NPC3, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '[LCV] Kill credit NPC 25847'),
+(@NPC3, 0, 1, 2, 8, 0, 100, 0, 46171, 0, 0, 0, 11, 34386, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '[LCV] Active Skill Create Hellfire Fire');
 
 DELETE FROM `conditions` WHERE `SourceEntry`=46171;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorTextId`,`ScriptName`,`Comment`)
