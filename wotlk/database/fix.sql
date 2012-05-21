c955a767aeb51bbe9765e4e9f5399ede59e6a2b0
4m1g0 <casadelblanco@gmail.com>
2012-05-21 23:22:56 +0200

    DB/quest: Corregida mision Ninguna piedad con los cautivos

--- wotlk/database/fix.sql
+++ wotlk/database/fix.sql
@@ -129,7 +129,11 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,6,0,0,0,100,0,1000,2000,18000,20000,11,19131,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,'[LCV] Cast Shield Charge');
 
 
--- Quest Protocolo de emergencia: sección 8.2, párrafo D   ID: 11796
+-- Author: Amnesio
+-- Date: 2012/04/06
+-- Quest: Protocolo de emergencia: sección 8.2, párrafo D   
+-- ID: 11796
+
 SET @NPC1 := 25845;
 SET @NPC2 := 25846;
 SET @NPC3 := 25847;
@@ -152,6 +156,8 @@ VALUES
 (13,1,46171,0,31,0,3,@NPC1,0,0,0, '','[LCV] Fyx Target 1 - Spell 46171'),
 (13,1,46171,1,31,0,3,@NPC2,0,0,0, '','[LCV] Fyx Target 2 - Spell 46171'),
 (13,1,46171,2,31,0,3,@NPC3,0,0,0, '','[LCV] Fyx Target 3 - Spell 46171');
+
+
 
 -- EXPERIMENTAL!! (more blizzlike dmg multipliers to icc)
 -- Lord Marrowgar
@@ -202,3 +208,170 @@ UPDATE `creature_template` SET `dmg_multiplier`=120 WHERE `entry`=38267;
 -- fix damage for sindra 10
 UPDATE `creature_template` SET `mindmg`=509, `maxdmg`=683, `attackpower`=805 WHERE `entry`=36853;
 
+
+
+-- Author: Decode --
+-- Date: 2012/04/13
+-- Quest: Ninguna piedad con los cautivos
+-- ID: 12245
+ 
+-- Insert into gossip_menu_option 
+ DELETE FROM `gossip_menu_option` WHERE  `menu_id`=27381;
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `box_coded`, `box_money`, `box_text`) VALUES
+(27381, 0, 0, 'No, Canciller, no diría que he venido a "rescatarle" per se.', 1, 1, 0, 0, 0, 0, NULL),
+(27381, 1, 0, '¿Dónde mantienen cautivo al escribano jefe Barriga?', 1, 1, 101, 0, 0, 0, NULL),
+(27381, 2, 0, '¿Viste adónde se llevaron al ingeniero sanitario Burke?', 1, 1, 103, 0, 0, 0, NULL),
+(27381, 3, 0, '¿Alguna idea de dónde tienen al guardia de la Muerte Schneider?', 1, 1, 102, 0, 0, 0, NULL);
+ 
+DELETE FROM `gossip_menu_option` WHERE  `menu_id`=27379;
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `box_coded`, `box_money`, `box_text`) VALUES
+(27379, 0, 0, 'No temas. Tus días como ingeniero sanitario se acercan a su fín.', 1, 1, 0, 0, 0, 0, NULL),
+(27379, 1, 0, '¿Puedes decirme dónde tienen enjaulado al Canciller Amai?', 1, 1, 100, 0, 0, 0, NULL),
+(27379, 2, 0, '¿Dónde mantienen cautivo al escribano jefe Barriga?', 1, 1, 101, 0, 0, 0, NULL),
+(27379, 3, 0, '¿Alguna idea de dónde tienen al guardia de la Muerte Schneider?', 1, 1, 102, 0, 0, 0, NULL);
+ 
+DELETE FROM `gossip_menu_option` WHERE  `menu_id`=27378;
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `box_coded`, `box_money`, `box_text`) VALUES
+(27378, 0, 0, '¡Hoy no, escribano jefe!', 1, 1, 0, 0, 0, 0, NULL),
+(27378, 1, 0, '¿Puedes decirme dónde tienen enjaulado al Canciller Amai?', 1, 1, 100, 0, 0, 0, NULL),
+(27378, 2, 0, '¿Viste adónde se llevaron al ingeniero sanitario Burke?', 1, 1, 103, 0, 0, 0, NULL),
+(27378, 3, 0, '¿Alguna idea de dónde tienen al guardia de la Muerte Schneider?', 1, 1, 102, 0, 0, 0, NULL);
+ 
+DELETE FROM `gossip_menu_option` WHERE  `menu_id`=27376;
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `box_coded`, `box_money`, `box_text`) VALUES
+(27376, 0, 0, 'Me temo que no, Schneider. ¡Ha llegado tu hora!', 1, 1, 0, 0, 0, 0, NULL),
+(27376, 1, 0, '¿Puedes decirme dónde tienen enjaulado al Canciller Amai?', 1, 1, 100, 0, 0, 0, NULL),
+(27376, 2, 0, '¿Dónde mantienen cautivo al escribano jefe Barriga?', 1, 1, 101, 0, 0, 0, NULL),
+(27376, 3, 0, '¿Viste adónde se llevaron al ingeniero sanitario Burke?', 1, 1, 103, 0, 0, 0, NULL);
+
+ -- Creature text entry for Npc
+DELETE FROM creature_text WHERE entry=27376;
+INSERT INTO creature_text (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (27376,0,0,'¿Qué quieres decir con que ha llegado mi hora? ¡Te mataré ahí mismo!',12,0,100,0,0,0,'Comment');
+DELETE FROM creature_text WHERE entry=27381;
+INSERT INTO creature_text (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (27381,0,0,'¿Qué significa esto? ¡para! ¡te pagaré el doble!',12,0,100,0,0,0,'Comment');
+DELETE FROM creature_text WHERE entry=27379;
+INSERT INTO creature_text (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (27379,0,0,'No! ¡Te lo ruego! ¡Por favor, no me mates!',12,0,100,0,0,0,'Comment');
+DELETE FROM creature_text WHERE entry=27378;
+INSERT INTO creature_text (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (27378,0,0,'No puedes hablar en serio... ¡ Te amonestaré por esto $C!',12,0,100,0,0,0,'Comment');
+
+-- gossip_menu
+-- SELECT * FROM gossip_menu where entry IN (100 ,101 ,102 ,103,27376,27378,27379,27381)
+DELETE FROM gossip_menu WHERE entry IN (100 ,101 ,102 ,103,27376,27378,27379,27381);
+INSERT INTO gossip_menu (entry, text_id) VALUES
+(27376,8000000),
+(27378,8000001),
+(27379,8000002),
+(27381,8000003),
+(103,8000005),
+(102,8000006),
+(101,8000007),
+(100,8000008);
+
+-- Npc anwers to questions
+-- select * from npc_text WHERE id IN (8000000, 8000001, 8000002, 8000003, 8000005, 8000006, 8000007, 8000008);
+DELETE FROM npc_text WHERE id IN (8000000, 8000001, 8000002, 8000003, 8000005, 8000006, 8000007, 8000008);
+INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`, `WDBVerified`) VALUES
+(8000000, 'Crees que podrías sacarme de aquí, $c?', '', 0, 1, 0, 6, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000001, 'ah bien, has venido a rescatarme. ¡Ahora sacame de aquí!', '', 0, 1, 0, 5, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000002, 'Hey, $g tío. : ma\am;. te he visto por aquí. ¿ Te importaría sacarme de aquí?', '', 0, 1, 0, 1, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000003, 'No sé qué pasó. ¡Acababa de llegar a Rencor Venenoso y, de repente, me desperté en esta jaula! ¿ Has venido a rescatarme?', '', 0, 1, 0, 5, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000005, 'Creo que está en el lateral sur de la abadía.', 'Creo que está en el lateral sur de la abadía.', 0, 1, 0, 1, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000006, 'Creo que la tienen cerca de la Horca.', 'Creo que la tienen cerca de la Horca.', 0, 1, 0, 1, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000007, 'Creo que lo han enjaulado cerca del aserradero.', 'Creo que lo han enjaulado cerca del aserradero.', 0, 1, 0, 1, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(8000008, 'Creo que los vi llevarlo hacia la parte norte de la abadía, cerca de los objetivos de tiro con arco.', 'Creo que los vi llevarlo hacia la parte norte de la abadía, cerca de los objetivos de tiro con arco.', 0, 1, 0, 1, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+ -- Deathguard Schneider -------------
+SET @ENTRY := 27376;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI", gossip_menu_id=27376 WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,62,0,100,0,27376,0,0,0,80,27376000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0," [LCV] On gossip Menu select");
+
+SET @ENTRY := 27376000;
+SET @SOURCETYPE := 9;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,100,100,0,0,72,0,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Close Gossip to enter IC"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,100,100,0,0,1,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Talk IC"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,2000,2000,0,0,2,14,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Set faction to 14"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2000,2000,0,0,46,1,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Move out to the cage for being able to attack");
+  
+-- Senior Scrivener Barriga -------------
+ SET @ENTRY := 27378;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName='SmartAI', gossip_menu_id=27378 WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,@SOURCETYPE,0,0,62,0,100,0,27378,0,0,0,80,27378000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] On gossip Menu select");
+ 
+SET @ENTRY := 27378000;
+SET @SOURCETYPE := 9;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,100,100,0,0,72,0,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Close Gossip to enter IC"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,100,100,0,0,1,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Talk IC"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,2000,2000,0,0,2,14,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Set faction to 14"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2000,2000,0,0,46,1,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Move out to the cage for being able to attack");
+ 
+ -- Engineer Burke
+ SET @ENTRY := 27379;
+SET @SOURCETYPE := 0;
+
+ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName='SmartAI', gossip_menu_id=27379 WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,@SOURCETYPE,0,0,62,0,100,0,27379,0,0,0,80,27379000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] On gossip Menu select");
+ 
+SET @ENTRY := 27379000;
+SET @SOURCETYPE := 9; 
+ 
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,100,100,0,0,72,0,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Close Gossip to enter IC"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,100,100,0,0,1,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Talk IC"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,2000,2000,0,0,2,14,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Set faction to 14"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2000,2000,0,0,46,1,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Move out to the cage for being able to attack");
+ 
+ -- Chancellor Amai
+ SET @ENTRY := 27381;
+SET @SOURCETYPE := 0;
+ 
+DELETE FROM smart_scripts WHERE entryorguid=@ENTRY AND source_type=@SOURCETYPE;
+UPDATE creature_template SET AIName='SmartAI', gossip_menu_id=27381 WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,@SOURCETYPE,0,0,62,0,100,0,27381,0,0,0,80,27381000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] On gossip Menu select");
+ 
+SET @ENTRY := 27381000;
+SET @SOURCETYPE := 9;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,100,100,0,0,72,0,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Close Gossip to enter IC"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,100,100,0,0,1,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Talk IC"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,2000,2000,0,0,2,14,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Set faction to 14"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2000,2000,0,0,46,1,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"[LCV] Move out to the cage for being able to attack");
+ 
+-- Insert into conditions 
+DELETE FROM conditions WHERE SourceTypeOrReferenceId=15 AND SourceGroup IN (27376,27378,27379,27381);
+INSERT INTO conditions (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(15, 27381, 3, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27381, 2, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27381, 1, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27381, 0, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27379, 3, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27379, 2, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27379, 1, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27379, 0, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27378, 3, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27378, 2, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27378, 1, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27378, 0, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27376, 3, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27376, 2, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27376, 1, 0, 9, 12245, 0, 0, 0, '', NULL),
+(15, 27376, 0, 0, 9, 12245, 0, 0, 0, '', NULL);
